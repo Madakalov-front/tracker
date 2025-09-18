261c8fb06e4876406fcaf86b82cfdf99f9fdf03e
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-09-18 16:06:51 +0500

    dasadsdasdasd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
-import { connectDB } from './config/db';
-import app from './app';
-import dotenv from 'dotenv'
+import { connectDB } from "./config/db";
+import app from "./app";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log("Server started");
+    });
 });
